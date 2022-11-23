5f1ae9d86fa23e1ec1652b12345cea343f7654c9
yusrilap <yusrilazharpermana@gmail.com>
2022-11-23 17:40:53 +0700

    Commit Login

---
+++ lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:hms_16/screens/splash/splash_screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+};
