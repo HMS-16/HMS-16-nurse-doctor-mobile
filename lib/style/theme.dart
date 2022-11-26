ffe121818036c538af530d6dd4fd9e0e06cd8353
prayogidcp <prayogidwicahyoputra@gmail.com>
2022-11-26 18:26:35 +0700

    add theme style dan image assets

---
+++ lib/style/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color
+Color cIcon = const Color.fromRGBO(50, 50, 50, 1);
+Color cBlackBase = const Color.fromRGBO(34, 34, 34, 1);
+Color cBlackLighter = const Color.fromRGBO(61, 61, 61, 1);
+Color cBlackLight = const Color.fromRGBO(46, 46, 46, 1);
+Color cBlackLightest = const Color.fromRGBO(84, 84, 84, 1);
+Color cWhiteBase = const Color.fromRGBO(253, 253, 253, 1);
+Color cSecondaryLightest = const Color.fromRGBO(244, 248, 253, 1);
+Color cWhiteDarker = const Color.fromRGBO(227, 227, 227, 1);
+Color cInfoLightest = const Color.fromRGBO(171, 227, 239, 1);
+Color cInfoLight = const Color.fromRGBO(86, 200, 222, 1);
+Color cPrimaryBase = const Color.fromRGBO(17, 83, 181, 1);
+Color cSuccessBase = const Color.fromRGBO(41, 153, 29, 1);
+
+TextStyle textStyle = GoogleFonts.nunito();
