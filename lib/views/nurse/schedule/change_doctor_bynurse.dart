e1591538f92ae1a7a94d1b9386bb65206f1d4254
prayogidcp <prayogidwicahyoputra@gmail.com>
2022-11-26 18:50:38 +0700

    add change detail view schedule

---
+++ lib/views/nurse/schedule/change_doctor_bynurse.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hms_16/Views/nurse/schedule/view_schedule_bynurse.dart';
+import 'package:hms_16/style/theme.dart';
+import 'package:intl/intl.dart';
+
+class ChangeDoctorByNurse extends StatelessWidget {
+  const ChangeDoctorByNurse({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              leading: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: cIcon,
+                  )),
+              title: Text("Change Doctor",
+                  style: textStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: cBlackBase)),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Today's Schedule (Avaible Doctor)",
+                    style: textStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                  Text(
+                    DateFormat('EEE, d MMM y').format(selectedDate),
+                    style: textStyle.copyWith(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: cBlackLightest),
+                  ),
+                  const SizedBox(
+                    height: 17.0,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 20, bottom: 20),
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 10,
+                          )
+                        ],
+                        borderRadius: BorderRadius.circular(15),
+                        color: cSecondaryLightest),
+                    child: ListTile(
+                        onTap: () async {
+                          await showDialog<void>(
+                            context: context,
+                            barrierDismissible: true,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text(
+                                  'Change Doctor to Dr. Nastasya?',
+                                  textAlign: TextAlign.center,
+                                  style: textStyle.copyWith(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                actions: <Widget>[
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      SizedBox(
+                                        width: 120,
+                                        child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            backgroundColor: cPrimaryBase,
+                                          ),
+                                          onPressed: () async {
+                                            await showDialog<void>(
+                                              context: context,
+                                              barrierDismissible: true,
+                                              builder: (BuildContext context) {
+                                                return AlertDialog(
+                                                  title: Text(
+                                                    'Doctor has been successfully changed!',
+                                                    textAlign: TextAlign.center,
+                                                    style: textStyle.copyWith(
+                                                        fontSize: 16,
+                                                        fontWeight:
+                                                            FontWeight.w600),
+                                                  ),
+                                                  content: Image.asset(
+                                                      "assets/images/winner_people_flat_icons.jpeg"),
+                                                  actions: <Widget>[
+                                                    ElevatedButton(
+                                                      style: ElevatedButton
+                                                          .styleFrom(
+                                                              backgroundColor:
+                                                                  cPrimaryBase,
+                                                              fixedSize: Size
+                                                                  .fromWidth(
+                                                                MediaQuery.of(
+                                                                        context)
+                                                                    .size
+                                                                    .width,
+                                                              )),
+                                                      onPressed: () {
+                                                        Navigator.pop(context);
+                                                      },
+                                                      child: const Text("Ok"),
+                                                    ),
+                                                  ],
+                                                );
+                                              },
+                                            );
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text(
+                                            "Yes",
+                                            style: textStyle.copyWith(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w600,
+                                                color: Colors.white),
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 120,
+                                        child: OutlinedButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text(
+                                            "No",
+                                            style: textStyle.copyWith(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w600,
+                                                color: cBlackBase),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        leading: const SizedBox(
+                            width: 30, height: 30, child: CircleAvatar()),
+                        title: Text(
+                          "Dr. Nastasya",
+                          style: textStyle.copyWith(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: cBlackBase),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Clinical Specialist Doctor",
+                              style: textStyle.copyWith(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12,
+                                  color: cBlackLightest),
+                            ),
+                            const SizedBox(
+                              height: 20.0,
+                            ),
+                            Text(
+                              "1.30 pm - 2.30 pm",
+                              style: textStyle.copyWith(
+                                  color: cBlackLightest,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12),
+                            )
+                          ],
+                        ),
+                        trailing: Icon(
+                          Icons.message,
+                          color: cInfoLight,
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/nurse/schedule/change_schedule_bynurse.dart
@@ -0,0 +1,268 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hms_16/Views/nurse/schedule/view_schedule_bynurse.dart';
+import 'package:hms_16/style/theme.dart';
+import 'package:intl/intl.dart';
+
+class ChangeScheduleByNurse extends StatefulWidget {
+  const ChangeScheduleByNurse({super.key});
+
+  @override
+  State<ChangeScheduleByNurse> createState() => _ChangeScheduleByNurseState();
+}
+
+List<String> itemsSchedule = [
+  "1.30 pm - 2.30 pm",
+  "2.45 pm - 3.45 pm",
+  "6.00 pm - 7.00 pm",
+  "7.20 pm - 8.20 pm"
+];
+String valueDropdown = itemsSchedule.first;
+
+class _ChangeScheduleByNurseState extends State<ChangeScheduleByNurse> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              leading: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: cIcon,
+                  )),
+              title: Text("Schedule Available",
+                  style: textStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: cBlackBase)),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    DateFormat('EEEE, MMMM d').format(selectedDate),
+                    style: textStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: cBlackBase,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "Specialist Nurse",
+                    style: textStyle.copyWith(
+                        fontWeight: FontWeight.w700, fontSize: 12),
+                  ),
+                  SizedBox(
+                    height: 43,
+                    child: TextFormField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                          contentPadding:
+                              const EdgeInsets.only(top: 12, left: 12),
+                          filled: true,
+                          fillColor: cWhiteDarker,
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(12)),
+                          hintText: "Clinical Specialist",
+                          hintStyle: textStyle.copyWith(
+                              fontSize: 14, fontWeight: FontWeight.w400)),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "Status",
+                    style: textStyle.copyWith(
+                        fontWeight: FontWeight.w700, fontSize: 12),
+                  ),
+                  SizedBox(
+                    height: 43,
+                    child: TextFormField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                          contentPadding:
+                              const EdgeInsets.only(top: 12, left: 12),
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(12)),
+                          fillColor: cWhiteDarker,
+                          hintText: "Available",
+                          hintStyle: textStyle.copyWith(
+                              fontSize: 14, fontWeight: FontWeight.w400)),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "Hours Available",
+                    style: textStyle.copyWith(
+                        fontWeight: FontWeight.w700, fontSize: 12),
+                  ),
+                  DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                        fillColor: cSecondaryLightest,
+                        filled: true,
+                        enabledBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide.none)),
+                    isExpanded: true,
+                    value: valueDropdown,
+                    elevation: 0,
+                    borderRadius: BorderRadius.circular(12),
+                    style: textStyle.copyWith(
+                        fontSize: 14, fontWeight: FontWeight.w400),
+                    onChanged: (value) {
+                      setState(() {
+                        valueDropdown = value!;
+                      });
+                    },
+                    items: itemsSchedule
+                        .map<DropdownMenuItem<String>>(
+                          (e) => DropdownMenuItem(
+                            value: e,
+                            child: Text(e),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(cSuccessBase)),
+                      onPressed: () async {
+                        await showDialog<void>(
+                          context: context,
+                          barrierDismissible: true,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              content: SingleChildScrollView(
+                                  child: ListTile(
+                                leading: const SizedBox(
+                                    height: 30,
+                                    width: 30,
+                                    child: CircleAvatar()),
+                                title: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Schedule Available',
+                                        style: textStyle.copyWith(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 16)),
+                                    Text(
+                                        DateFormat('EEEE, MMMM d')
+                                            .format(selectedDate),
+                                        style: textStyle.copyWith(
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 12,
+                                            color: cBlackLightest)),
+                                    Text(valueDropdown,
+                                        style: textStyle.copyWith(
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 12,
+                                            color: cBlackLightest)),
+                                    const SizedBox(
+                                      height: 20.0,
+                                    ),
+                                    Text(
+                                        "Your schedule has been successfully changed!",
+                                        style: textStyle.copyWith(
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 12,
+                                            color: cBlackLightest)),
+                                  ],
+                                ),
+                              )),
+                              actions: <Widget>[
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: cPrimaryBase,
+                                    ),
+                                    onPressed: () async {
+                                      await showDialog<void>(
+                                        context: context,
+                                        barrierDismissible: true,
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            title: Text(
+                                              'Schedule has been successfully changed!',
+                                              textAlign: TextAlign.center,
+                                              style: textStyle.copyWith(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                            content: Image.asset(
+                                                "assets/images/winner_people_flat_icons.jpeg"),
+                                            actions: <Widget>[
+                                              ElevatedButton(
+                                                style: ElevatedButton.styleFrom(
+                                                    backgroundColor:
+                                                        cPrimaryBase,
+                                                    fixedSize: Size.fromWidth(
+                                                      MediaQuery.of(context)
+                                                          .size
+                                                          .width,
+                                                    )),
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: const Text("Ok"),
+                                              ),
+                                            ],
+                                          );
+                                        },
+                                      );
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text("Save Schedule Changes",
+                                        style: textStyle.copyWith(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600,
+                                            color: Colors.white)),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      child: Text(
+                        "Save",
+                        style: textStyle.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
