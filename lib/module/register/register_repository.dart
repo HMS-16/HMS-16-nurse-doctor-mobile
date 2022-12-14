545568687122f07df49b37a92857b45e966aae1e
prayogidcp <prayogidwicahyoputra@gmail.com>
2022-12-15 03:37:09 +0700

    fixing validasi on sign up

--- lib/module/register/register_repository.dart
+++ lib/module/register/register_repository.dart
@@ -2,17 +2,21 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:hms_16/api/auth.dart';
 import 'package:hms_16/model/register_model.dart';
+import 'package:hms_16/widget/duration_dialog.dart';
 
 class RegisterViewModel with ChangeNotifier {
   String message = '';
 
   Future<void> register(Datum data, String token, BuildContext context) async {
     try {
-      var response = await Auth().register(data: data,token: token);
+      var response = await Auth().register(data: data, token: token);
       message = response.data['message'].toString();
+      print(data.toJson());
       print(message);
-      ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text("create account success")));
+      durationDialog(
+          context, "New user account has been registered successfully");
+      // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      //     content: Text("New user account has been registered successfully")));
     } on DioError catch (e) {
       message = e.response!.data;
       print(message);
